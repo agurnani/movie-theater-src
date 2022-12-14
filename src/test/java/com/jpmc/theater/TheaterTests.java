ac64ad832bc94c58f532a227eca10eff45d4a8e2
Anil Gurnani <anil.gurnani@gmail.com>
2022-12-13 20:26:10 -0500

    fixed TheaterTests

--- src/test/java/com/jpmc/theater/TheaterTests.java
+++ src/test/java/com/jpmc/theater/TheaterTests.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,17 +29,18 @@ class TheaterTests {
 
     @Test
     void printMovieSchedule() {
-        final String expectedOutput = "2022-12-12\r\n" +
+        LocalDate ld = LocalDateProvider.singleton().currentDate();
+        final String expectedOutput = ld+"\r\n" +
                 "===================================================\r\n" +
-                "1: 2022-12-12T09:00 Turning Red (1 hour 25 minutes) $11.0\r\n" +
-                "2: 2022-12-12T11:00 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
-                "3: 2022-12-12T12:50 The Batman (1 hour 35 minutes) $9.0\r\n" +
-                "4: 2022-12-12T14:30 Turning Red (1 hour 25 minutes) $11.0\r\n" +
-                "5: 2022-12-12T16:10 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
-                "6: 2022-12-12T17:50 The Batman (1 hour 35 minutes) $9.0\r\n" +
-                "7: 2022-12-12T19:30 Turning Red (1 hour 25 minutes) $11.0\r\n" +
-                "8: 2022-12-12T21:10 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
-                "9: 2022-12-12T23:00 The Batman (1 hour 35 minutes) $9.0\r\n" +
+                "1: "+ld+"T09:00 Turning Red (1 hour 25 minutes) $11.0\r\n" +
+                "2: "+ld+"T11:00 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
+                "3: "+ld+"T12:50 The Batman (1 hour 35 minutes) $9.0\r\n" +
+                "4: "+ld+"T14:30 Turning Red (1 hour 25 minutes) $11.0\r\n" +
+                "5: "+ld+"T16:10 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
+                "6: "+ld+"T17:50 The Batman (1 hour 35 minutes) $9.0\r\n" +
+                "7: "+ld+"T19:30 Turning Red (1 hour 25 minutes) $11.0\r\n" +
+                "8: "+ld+"T21:10 Spider-Man: No Way Home (1 hour 30 minutes) $12.5\r\n" +
+                "9: "+ld+"T23:00 The Batman (1 hour 35 minutes) $9.0\r\n" +
                 "===================================================\r\n";
 
         Theater theater = new Theater(LocalDateProvider.singleton());
